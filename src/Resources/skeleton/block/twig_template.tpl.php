8d03d18f208a2fc73174e3bc0aa9a3c975099d30
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-08-26 15:24:22 +0200

    fix state usage

--- src/Resources/skeleton/block/twig_template.tpl.php
+++ src/Resources/skeleton/block/twig_template.tpl.php
@@ -1,8 +1,5 @@
 <ul>
     <li>Your block at <code><?php
-
-declare(strict_types=1);
-
     echo $helper->getFileLink(sprintf('%s/%s', $root_directory, $block_path), sprintf('%s', $block_path)); ?></code></li>
     <li>Your template at <code><?php echo $helper->getFileLink(sprintf('%s/%s', $root_directory, $relative_path), sprintf('%s', $relative_path)); ?></code></li>
 </ul>
